a330e853ece0699c07c522e13f1f1943c327075f
Johnny-Klas <giannis.klas@gmail.com>
2024-08-10 02:36:21 +0300

    projects section functionality added

--- hamburger.js
+++ hamburger.js
@@ -1,30 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.getElementById('menu-toggle');
-    const nav = document.getElementById('nav');
-  
-    // Toggle menu visibility
-    menuToggle.addEventListener('click', function() {
-      nav.classList.toggle('show');
-    });
-  
-    // Function to close menu on window resize
-    function handleResize() {
-      if (window.innerWidth > 768) {
-        // On larger screens, ensure the menu is visible and reset
-        nav.classList.remove('show');
-      }
-    }
+let angle = 0;
 
-    document.addEventListener('click', function(event) {
-        if (!nav.contains(event.target) && !menuToggle.contains(event.target)) {
-          nav.classList.remove('show');
-        }
-      });
-  
-    // Add event listener for window resize
-    window.addEventListener('resize', handleResize);
-  
-    // Initial check on page load
-    handleResize();
-  });
-  
+function rotateCarousel(rotation) {
+    angle += rotation;
+    document.querySelector('.carousel').style.transform = `rotateY(${angle}deg)`;
+}
